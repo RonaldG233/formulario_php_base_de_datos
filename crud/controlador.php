98d49aa2683ddef779d0770688c7b07fbd5443a1
ronaldG <ronaldalejandrogallocastellano@gmail.com>
2024-11-19 11:58:40 -0500

    avance php

---
+++ crud/controlador.php
@@ -0,0 +1,59 @@
+<?php
+
+// echo "<pre>";
+// print_r($_REQUEST); 
+// echo "</pre>";
+
+require('conexion.php');
+
+$db=new Conexion();
+$conexion=$db->getConexion();
+
+$nombre=$_POST['nombre'];
+$apellido=$_POST['apellido'];
+$correo=$_POST['correo'];
+$fecha_nac=$_POST['fecha_nac'];
+$genero_id=$_POST['genero_id'];
+$ciudad_id=$_POST['ciudad_id'];
+$lenguajes=$_POST['nombre_lenguaje_id'];
+
+
+
+$sql="INSERT INTO USUARIOS (nombre_usuario,apellido_usuario,correo_usuario,fecha_nacimiento_usuario,genero,ciudad)
+ VALUES (:nombre_usuario,:apellido_usuario,:correo_usuario,:fecha_nacimiento_usuario,:genero_id,:ciudad_id)";
+ 
+$stm=$conexion ->prepare($sql);
+$stm ->bindParam(":nombre_usuario",$nombre);
+$stm ->bindParam(":apellido_usuario",$apellido);
+$stm ->bindParam(":correo_usuario",$correo);
+$stm ->bindParam(":fecha_nacimiento_usuario",$fecha_nac);
+$stm ->bindParam(":genero_id",$genero_id);
+$stm ->bindParam(":ciudad_id",$ciudad_id);
+
+
+
+
+$usuario= $stm ->execute();
+$id_usuario=$conexion->lastInsertId();
+var_dump($id_usuario);
+
+echo "<pre>";
+print_r($_REQUEST);
+echo "</pre>";
+
+$sql4="INSERT INTO LENGUAJE_USUARIO (nombre_usuario,nombre_lenguaje)
+VALUES (:nombre_usuario,:nombre_lenguaje)";
+$stm4=$conexion ->prepare($sql4);{
+    foreach ($lenguajes as $key => $value) {
+        $stm4->bindParam(":nombre_usuario",$id_usuario);
+        $stm4->bindParam(":nombre_lenguaje",$value);
+        $stm4->execute();
+    }
+    
+
+}
+
+
+
+
+
--- crud/index.php
+++ crud/index.php
@@ -8,16 +8,52 @@ $sql="SELECT* FROM ciudades";
 $bandera=$conexion->prepare($sql);
 $bandera ->execute();
 $ciudades=$bandera->fetchAll();
-?>
 
-<form action="" method="post">
+$sql2="SELECT* FROM generos";
+$bandera2=$conexion->prepare($sql2);
+$bandera2 ->execute();
+$generos=$bandera2->fetchAll();
+
+$sql3="SELECT* FROM lenguajes";
+$bandera3=$conexion->prepare($sql3);
+$bandera3 ->execute();
+$lenguajes=$bandera3->fetchAll();
+
+?>
+<fieldset>
+<form action="controlador.php" method="post">
+
     <div>
-        <label for="ciudad_id">ciudad</label>
+        <h3>REGISTRO DE USUARIOS</h3>
+    </div>
+    <div>
+        <label for="nombre">nombre: </label>
+        <input type="text" name="nombre">
+    </div>  
+    <br>
+    <div>
+        <label for="apellido">apellido: </label>
+        <input type="text" name="apellido" required>
+    </div>
+    <br>
+    <div>
+        <label for="correo">correo: </label>
+        <input type="text" name="correo">
+    </div>
+    <br>
+    <div>
+        <label for="fecha_nac">fecha de nacimiento: </label>
+        <input type="date" name="fecha_nac">
+    </div>  
+    <br>
+
+    <div>
+        <label for="ciudad_id">ciudades</label>
         <select name="ciudad_id" id="ciudad_id">
             <?php
             foreach ($ciudades as $key => $value){
                 ?>
-                <option id="<?= $value['id'] ?>">
+                <option id="<?= $value['id'] ?>" value="<?= $value['id'] ?>">
                     <?= $value['ciudad'] ?>
                 </option>
             <?php    
@@ -25,5 +61,54 @@ $ciudades=$bandera->fetchAll();
             ?>
         </select>
     </div>
+            <br>
+    <div>
+        <label for="genero_id">genero: </label>
+            <div>
+            <?php
+
+            foreach($generos as $key=> $value){
+                ?>
+                <label for="<?=$value['id'] ?>"  ><?=$value['genero']?></label>
+                <input type="radio" name="genero_id" id="<?=$value['id'] ?>"  value="<?=$value['id']?>"    >
+                
+                
+            <?php
+            }
+            ?>
+            </div>
+    </div>
+
+    <br>
+
+    <div>
+        <label for="lenguaje_id">Lenguajes: </label>
+            <div>
+            <?php
+
+            foreach($lenguajes as $key=> $value){
+                ?>
+                <label for="<?=$value['id'] ?>"  ><?=$value['nombre_lenguaje']?></label>
+                <input type="checkbox" name="nombre_lenguaje_id[]" id="<?=$value['id'] ?>"  value="<?=$value['id']?>"    >
+                
+                
+            <?php
+            }
+            ?>
+            </div>
+    </div>
+
+    <br>
+    <button>Guardar datos</button>
+    
+
 </form>
+</fieldset>
+
+
+
+
+
+
+
 
